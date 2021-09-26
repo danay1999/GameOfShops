4bb76c004d69923a49e7a66db6e7b5911b20fad1
unknown <dfern237@fiu.edu>
2021-09-26 05:04:55 -0400

    backend

--- api/api.py
+++ api/api.py
@@ -1,8 +1,18 @@
 import time
 from flask import Flask
+from os import environ as env
+from pymongo import MongoClient
+import pymongo
+import requests
+from connection import MONGO_CONNECTION_URI 
 
 app = Flask(__name__)
+client = pymongo.MongoClient('MONGO_CONNECTION_URI')
+db = client['Game_of_Shops']
+# overview = db.overview.find()
+print(db['Overview'])
 
 @app.route('/')
-def home():
-    return 0
+def main():
+    overview = db['Overview'].find()
+    return overview
